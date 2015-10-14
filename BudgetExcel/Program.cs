281b7b8297c262a4d38650094e4499f24dbff1ae
jgudgel <jgudgel@ucsd.edu>
2015-10-14 12:49:12 -0700

    Clean up input

--- BudgetExcel/Program.cs
+++ BudgetExcel/Program.cs
@@ -11,28 +11,28 @@ namespace BudgetExcel
     {
         static void Main(string[] args)
         {
-
             string category = "";
             double value;
 
-
-            Console.WriteLine("Purchase History Beta 1.0");
+            Console.WriteLine("                     Purchase History Beta 1.0");
             ExcelWriter ew = new ExcelWriter();
             do
             {
+                Console.WriteLine(" ---------------------------------------------------------------- ");
                 Console.WriteLine("Enter the Purchase Category(i.e. Food, Home, Entertainment, Transportation):");
                 category = Console.ReadLine();
                 Console.WriteLine("Enter the Purchase Value(i.e. 3.99, 8, 750.00):");
                 value = Convert.ToDouble(Console.ReadLine());
-
+                Console.WriteLine();
 
                 if (!ew.CreateExcelDoc())
                 {
                     ew.OpenExcelDoc();
                 }
                 ew.WriteToExcel(category, value);
-                Console.WriteLine("\nPress \"Enter\" to add another entry or \"Esc\" to end the Application\n");
 
+                Console.WriteLine("\nPress \"Enter\" to add another entry or \"Esc\" to end the Application\n");
+                            
             } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
             ew.Close();
         }
