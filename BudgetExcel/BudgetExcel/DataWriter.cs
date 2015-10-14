2c123aa756d309d0d36f8e1440e3079b82d331db
jgudgel <jgudgel@ucsd.edu>
2015-10-14 11:59:48 -0700

    first commit

---
+++ BudgetExcel/BudgetExcel/DataWriter.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BudgetExcel
+{
+    class ExcelWriter
+    {
+        Excel.Application xlApp = null;
+        Excel.Workbook xlWorkBook;
+        Excel.Worksheet xlWorkSheet;
+        object misValue;
+        int _RowIndex;
+        string _filePath = " ";
+        string _currentDate = "";
+        string _myDocPath = "";
+
+        public ExcelWriter()
+        {
+            _currentDate = DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString() + DateTime.Now.Year.ToString();
+            _myDocPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Budget.xls";
+        }
+
+        public void WriteToExcel(string category, double value)
+        {
+            //Logs
+            Console.WriteLine("Adding \"" + _currentDate + ", " + category + ", " + value + "\" to " + _myDocPath);
+
+            _RowIndex++;
+            xlWorkSheet.Cells[_RowIndex, 1] = _currentDate;
+            xlWorkSheet.Cells[_RowIndex, 2] = category;
+            xlWorkSheet.Cells[_RowIndex, 3] = value;
+
+            xlWorkBook.Save();
+            //_filePath = "E:\\Excel\\BudgetExcel\\Budget.xls";
+            //xlWorkBook.SaveAs(_filePath, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue,
+            //                    misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue,
+            //                    misValue, misValue, misValue, misValue);
+
+        }
+
+        public void OpenExcelDoc()
+        {
+            // Logs
+            Console.WriteLine("Opening Budget.xls...");
+            if (xlApp == null)
+            {
+                xlApp = new Microsoft.Office.Interop.Excel.Application();
+            }
+            if (xlApp == null)
+            {
+                Console.WriteLine("Excel is not properly installed!!");
+                return;
+            }
+
+            xlApp.DisplayAlerts = false;
+
+            misValue = System.Reflection.Missing.Value;
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            //_fileName = name;
+            _filePath = "E:\\Excel\\BudgetExcel\\Budget.xls";
+
+            //xlWorkBook = xlApp.Workbooks.Open(_filePath, misValue, false, Excel.XlFileFormat.xlWorkbookNormal, 
+            //                                    misValue, misValue, true,misValue, misValue, true,
+            //                                    misValue, misValue, misValue, misValue, misValue);
+            xlWorkBook = xlApp.Workbooks.Open(_myDocPath);
+
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            // Find index of next available cell
+            _RowIndex = xlWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell, Type.Missing).Row;
+
+            xlWorkBook.SaveAs(_myDocPath, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue,
+                                misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue,
+                                misValue, misValue, misValue, misValue);
+            //xlWorkBook.Save();
+        }
+
+        public bool CreateExcelDoc()
+        {
+            //check if file does not exists
+            Console.WriteLine("Checking if Budget.xls Exists...");
+            if (System.IO.File.Exists(_myDocPath))//"E:\\Excel\\BudgetExcel\\Budget.xls"
+            {
+                Console.WriteLine("Confirmed...");
+                return false;
+            }
+
+
+            // Logs
+            Console.WriteLine("Does not exist... Creating Budget.xls...");
+
+            xlApp = new Microsoft.Office.Interop.Excel.Application();
+            if (xlApp == null)
+            {
+                Console.WriteLine("Excel is not properly installed!!");
+                return false;
+            }
+
+            misValue = System.Reflection.Missing.Value;
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+
+            _filePath = "E:\\Excel\\BudgetExcel\\Budget.xls";
+            
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            _RowIndex = 1;
+            xlWorkSheet.Cells[_RowIndex, 1] = "Date";
+            xlWorkSheet.Cells[_RowIndex, 2] = "Category";
+            xlWorkSheet.Cells[_RowIndex, 3] = "Payment";
+
+            xlWorkBook.SaveAs(_myDocPath, Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, 
+                                misValue, misValue, Excel.XlSaveAsAccessMode.xlExclusive, misValue, 
+                                misValue, misValue, misValue, misValue);
+            return true;
+            //xlWorkBook.Save();
+        }
+
+        public void Close()
+        {
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            releaseObject(xlWorkSheet);
+            releaseObject(xlWorkBook);
+            releaseObject(xlApp);
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                Console.WriteLine("Exception Occured while releasing object " + ex.ToString());
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
---
+++ BudgetExcel/BudgetExcel/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace BudgetExcel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string category = "";
+            double value;
+
+
+            Console.WriteLine("Purchase History Beta 1.0");
+            ExcelWriter ew = new ExcelWriter();
+            do
+            {
+                Console.WriteLine("Enter the Purchase Category(i.e. Food, Home, Entertainment, Transportation):");
+                category = Console.ReadLine();
+                Console.WriteLine("Enter the Purchase Value(i.e. 3.99, 8, 750.00):");
+                value = Convert.ToDouble(Console.ReadLine());
+
+
+                if (!ew.CreateExcelDoc())
+                {
+                    ew.OpenExcelDoc();
+                }
+                ew.WriteToExcel(category, value);
+                Console.WriteLine("\nPress \"Enter\" to add another entry or \"Esc\" to end the Application\n");
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            ew.Close();
+        }
+    }
+}
